cf0282e02658d2fa1e22b2e1e0ada44b6c1070cc
ali-jalil <qzb1@txstate.edu>
2025-02-10 21:09:22 -0600

    SCRUM-16 Added input tag and example of where document will sit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +15,23 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>*/}
+        <div className='Input-Form'>
+          <label for="myfile">Select a file: </label>
+          <input type="file" id="myfile" name="myfile"/>
+          <input type="submit"/>
+        </div>
+        <div className='Doclist'>
+          <ul>
+            <li>Document-1</li>
+            <li>Document-2</li>
+            <li>Document-3</li>
+          </ul>
+          </div>
+
       </header>
+
+
     </div>
   );
 }
