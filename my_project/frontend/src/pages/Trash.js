4791b5ac01f1c77ae27b08f7a7514a54791f096a
ali-jalil <qzb1@txstate.edu>
2025-02-19 14:49:44 -0600

    SCRUM-14 Made files for TrashBucket and Home page

---
+++ my_project/frontend/src/pages/Trash.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrashBucket from '../components/TrashBucket/TrashBucket';
+
+const Trash = () => {
+  return (
+    <div className="trash-page">
+      <h1>Deleted Documents</h1>
+      <TrashBucket />
+    </div>
+  );
+};
+
+export default Trash;
