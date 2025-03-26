1091deaa7e392f3d6a21bb30ee9fe8113447e2b8
ali-jalil <qzb1@txstate.edu>
2025-03-26 13:12:01 -0500

    Scrum-50 did some upadte to the UI and moved files around

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders Home link', () => {
+test('renders header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Home/i);
+  const linkElement = screen.getByText(/Select a file/i);
   expect(linkElement).toBeInTheDocument();
 });
