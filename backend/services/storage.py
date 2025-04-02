a50a6800e3a4f775847ab8c5fa0b81bdc4e447aa
ali-jalil <qzb1@txstate.edu>
2025-04-02 14:26:13 -0500

    SCRUM-75 Put the CRUD into its own file

---
+++ backend/services/storage.py
@@ -0,0 +1,31 @@
+import os
+import json
+from config import DOC_STORE_FILE
+
+doc_store = {}
+doc_counter = 0
+
+def load_doc_store():
+    global doc_store, doc_counter
+    if os.path.exists(DOC_STORE_FILE):
+        try:
+            with open(DOC_STORE_FILE, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                doc_store.update(data.get('docs', {}))
+                doc_counter = data.get('counter', 0)
+        except:
+            pass
+
+def save_doc_store():
+    global doc_store, doc_counter
+    data = {
+        'docs': doc_store,
+        'counter': doc_counter
+    }
+    try:
+        with open(DOC_STORE_FILE, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
+    except:
+        pass
+
+load_doc_store()
