70809db00e4eb9e9bbb2b4e1de97ea5689e0bd34
ali-jalil <qzb1@txstate.edu>
2025-04-03 13:01:08 -0500

    SCRUM-75 changed eventlet to gevent in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,17 @@
 import os
-import eventlet # type: ignore
-from flask import Flask # type: ignore
+from gevent import monkey 
+from gevent.pywsgi import WSGIServer 
+from flask import Flask  # type: ignore
 from services.storage import load_doc_store, save_doc_store, doc_store
 from routes.document import get_audio_file, upload_audio  # type: ignore
 from routes.docmanage import list_docs, get_doc, create_doc, update_doc, delete_doc
 from routes.trash_route import get_trash_files, restore_file, get_upload_files
-from services.socketio_instance import socketio # type: ignore
+from services.socketio_instance import socketio  # type: ignore
 
 app = Flask(__name__)
 socketio.init_app(app)
 
-os.environ['EVENTLET_NO_GREENDNS'] = '1'
-eventlet.monkey_patch()
+monkey.patch_all()  # Patch standard library modules to work with gevent's green threads
 
 load_doc_store()
 
@@ -44,6 +44,7 @@ app.add_url_rule('/trash-files', view_func=get_trash_files, methods=['GET'])
 app.add_url_rule('/restore_file/<filename>', view_func=restore_file, methods=['GET'])
 app.add_url_rule('/upload-files', view_func=get_upload_files, methods=['GET'])
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+if __name__ == '__main__':  # Use gevent's WSGI server
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
 
