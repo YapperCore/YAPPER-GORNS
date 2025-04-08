944d465c80552d5a271d0ff779f5be80feea269f
Joshua Egwaikhide <ktz4@txstate.edu>
2025-04-07 20:42:28 -0500

    SCRUM-57 made login root

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,8 +15,7 @@ function App() {
       <div className="App">
         <nav style={{ background:'#333', color:'#fff', padding:'1rem' }}>
           
-          <Link to="/" style={{ color:'#fff', marginRight:'1rem' }}>Signup</Link>
-          <Link to="/login" style={{ color:'#fff', marginRight:'1rem' }}>Login</Link>
+          <Link to="/" style={{ color:'#fff', marginRight:'1rem' }}>Login</Link>
           <Link to="/home" style={{ color:'#fff', marginRight:'1rem' }}>Home</Link>
           <Link to="/trash" style={{ color:'#fff', marginRight:'1rem' }}>Trash</Link>
           <Link to="/docs" style={{ color:'#fff' }}>Docs</Link>
