831f4d0737fc2375618f5d75fe2318c548a4a22c
Ali Jalil <qzb1@txstate.edu>
2025-04-21 15:06:13 -0500

    Remove files now in .gitignore

--- frontend/src/__tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders header', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Select a file/i);
-  expect(linkElement).toBeInTheDocument();
-});
